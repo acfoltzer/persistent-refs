112a56b6cb5c21a8874c396d10cae42e0550a4e1
Adam C. Foltzer <acfoltzer@galois.com>
2013-02-09 17:56:41 -0800

    inlined TH splices for lens, due to bug with older TH and Any type

--- src/Control/Monad/ST/Persistent/Internal.hs
+++ src/Control/Monad/ST/Persistent/Internal.hs
@@ -2,6 +2,7 @@
 {-# LANGUAGE Rank2Types #-}
 {-# LANGUAGE TemplateHaskell #-}
 
+{-# OPTIONS_GHC -ddump-splices #-}
 module Control.Monad.ST.Persistent.Internal where
 
 import Control.Applicative (Applicative)
@@ -11,7 +12,19 @@ import Data.IntMap
 import GHC.Base
 
 data Heap = Heap { _heap :: IntMap Any, _next :: Int }
-makeLenses ''Heap
+-- makeLenses ''Heap
+
+-- manually using splices because TH before 7.6 can't handle Any
+heap :: Lens' Heap (IntMap Any)
+heap _f_a1T2 (Heap __heap'_a1T3 __next_a1T5)
+  = ((\ __heap_a1T4 -> Heap __heap_a1T4 __next_a1T5)
+     `fmap` (_f_a1T2 __heap'_a1T3))
+{-# INLINE heap #-}
+next :: Lens' Heap Int
+next _f_a1T6 (Heap __heap_a1T7 __next'_a1T8)
+  = ((\ __next_a1T9 -> Heap __heap_a1T7 __next_a1T9)
+     `fmap` (_f_a1T6 __next'_a1T8))
+{-# INLINE next #-}
 
 emptyHeap :: Heap
 emptyHeap = Heap { _heap = empty, _next = minBound }
